54bf0dd4e1a3614279f9d7d48ca28ca3ab500ec4
flyq <flyq951@gmail.com>
2020-03-01 17:04:18 +0800

    fmt main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,59 @@
-use jsonrpc_http_server::*;
 use jsonrpc_http_server::jsonrpc_core::*;
+use jsonrpc_http_server::*;
 use std::process::Command;
 
 fn main() {
-        let mut io = IoHandler::default();
-        io.add_method("say_hello", |_| {
-                let mut list_dir = Command::new("ls");
-                list_dir.status().expect("process failed to execute");
+    let mut io = IoHandler::default();
+    io.add_method("say_hello", |_| {
+        let mut list_dir = Command::new("ls");
+        list_dir.status().expect("process failed to execute");
 
+        Ok(Value::String("hello, ls".into()))
+    });
+
+    io.add_method("test", |_| {
+        let output = Command::new("mkdir")
+            .arg("book")
+            .output()
+            .expect("failed to execute process");
+
+        println!("status: {} ", output.status);
+
+        Ok(Value::String("test, sh".into()))
+    });
+
+    io.add_method("param_test", |params: Params| {
+        match params.parse::<(u64,)>() {
+            Ok((num,)) => Ok(Value::String(format!("world: {}", num))),
+            _ => Ok(Value::String("world".into())),
+        }
+    });
+
+    io.add_method("cita-cli", |params: Params| {
+        match params.parse::<Object(map)>() {
+            Ok(map) => {
+                let cita = Command::new("cita-cli")
+                    .arg(map.get("subcom1"))
+                    .arg(map.get("subcom2"))
+                    .arg("--code")
+                    .arg(map.get("code"))
+                    .arg("--private-key")
+                    .arg(map.get("private-key"))
+                    .arg("--address")
+                    .arg(mag.get("address"))
+                    .arg("--url http://127.0.0.1:1337");
                 Ok(Value::String("hello, ls".into()))
-        });
+            }
+            _ => Ok(Value::String("world".into())),
+        }
+    });
 
-	io.add_method("test", |_| {
-	        let output = Command::new("mkdir")
-        	    .arg("book")
-		    .output()
-		    .expect("failed to execute process");
-		    
- 	        println!("status: {} ", output.status);
-	    
-            Ok(Value::String("test, sh".into()))
-        });
+    let server = ServerBuilder::new(io)
+        .cors(DomainsValidation::AllowOnly(vec![
+            AccessControlAllowOrigin::Null,
+        ]))
+        .start_http(&"127.0.0.1:3030".parse().unwrap())
+        .expect("Unable to start RPC server");
 
-
-        io.add_method("param_test",  |params: Params| match params.parse::<(u64,)>() {
-                Ok((num,)) => Ok(Value::String(format!("world: {}", num))),
-                _ => Ok(Value::String("world".into())),
-        });
-
-        io.add_method("cita-cli",  |params: Params| match params.parse::<Object(map)>() {
-                Ok(map) => Ok(
-		    let cita = Command::new("cita-cli")
-		        .arg(map.get("subcom1"))
-			.arg(map.get("subcom2"))
-			.arg("--code")
-			.arg(map.get("code"))
-			.arg("--private-key")
-			.arg(map.get("private-key"))
-			.arg("--address")
-			.arg(mag.get("address"))
-			.arg("--url http://127.0.0.1:1337");
-			
-
-			),
-                _ => Ok(Value::String("world".into())),
-        });
-
-
-
-        let server = ServerBuilder::new(io)
-                .cors(DomainsValidation::AllowOnly(vec![AccessControlAllowOrigin::Null]))
-                .start_http(&"127.0.0.1:3030".parse().unwrap())
-                .expect("Unable to start RPC server");
-
-        server.wait();
+    server.wait();
 }
